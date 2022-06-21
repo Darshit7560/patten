0e64d7968ba7dc71a676a5cc8c540de8624276a3
Darshit7560 <100226377+Darshit7560@users.noreply.github.com>
2022-06-21 09:50:17 +0530

    Add files via upload

---
+++ Telusco.java
@@ -0,0 +1,318 @@
+
+public class Telusco 
+{
+  public static void main(String[] args)
+  {
+	  //for t
+	for(int i=1;i<=7;i++)
+	{
+		
+		
+		for(int j=1;j<=7;j++)
+		{
+			
+			if(i==1||j==4)
+			{
+				System.out.print(" *");	
+			}
+			else
+			{
+				System.out.print("  ");
+
+			}
+				
+			
+			
+		}
+		System.out.println();
+		
+			
+  
+		
+	}
+	System.out.println("\n");
+	
+	//for e
+	for(int a=1;a<=7;a++)
+	{
+		for(int b=1;b<=7;b++)
+		{
+			
+			if(b==1||a==1||a==4|a==7)
+			{
+				System.out.print("*");
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//for l
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||c==7)
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	
+	//for u
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||d==7||c==7)
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	
+	//for s
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(c==1||c==7||c==4||d==1&&c<4||d==7&&c>4)
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//for k
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||c==1||c==7||c==2&&d<6||c==3&&d<4||c==6&&d<6||c==5&&d<4)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print("  ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//for o
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||d==7||c==1||c==7)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	System.out.println();
+	
+	//for i
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(c==1||c==7||d==4)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//for n
+	for(int c=1;c<=7;c++)
+	{
+
+	
+		for(int d=1;d<=7;d++)
+		{
+			
+			if(d==1||d==7||d==2&&c<3&&c>1||d==3&&c<4&&c>2||d==4&&c<5&&c>3||d==5&&c<6&&c>4||d==6&&c<7&&c>5)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	
+	System.out.println();
+	//for e
+	for(int a=1;a<=7;a++)
+	{
+		for(int b=1;b<=7;b++)
+		{
+			
+			if(b==1||a==1||a==4|a==7)
+			{
+				System.out.print("*");
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	System.out.println();
+	
+	// for u
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||d==7||c==7)
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	
+	System.out.println();
+	
+	//for r
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||c==1||d==7&&c<5||c==4||c==5&&d==3&&d<4||c==6&&d==5&&d<6||c==7&&d==7&&d<=7)
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+		
+		
+		
+	}
+	System.out.println();
+	
+	//for o
+	for(int c=1;c<=7;c++)
+	{
+		for(int d=1;d<=7;d++)
+		{
+			if(d==1||d==7||c==1||c==7)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+	
+	//for n
+	for(int c=1;c<=7;c++)
+	{
+
+	
+		for(int d=1;d<=7;d++)
+		{
+			
+			if(d==1||d==7||d==2&&c<3&&c>1||d==3&&c<4&&c>2||d==4&&c<5&&c>3||d==5&&c<6&&c>4||d==6&&c<7&&c>5)
+				
+			{
+				System.out.print("*");	
+			}
+			else
+			{
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	
+	
+	
+
+
+	
+
+
+
+  }
+  
+  
+  
+}
+
+
